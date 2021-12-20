561cf36a03acfbfe18a3437987fcc44eaa39f61b
XC0D3-X <harithsaufi0707@gmail.com>
2021-12-20 21:33:45 +0800

    Update mwg.sh

--- mwg.sh
+++ mwg.sh
@@ -10,21 +10,21 @@ if [ "$(systemd-detect-virt)" == "openvz" ]; then
 		exit 1
 fi
 clear
-
+echo -e " ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰" |lolcat
 echo -e ""
-echo -e " ╔══════════════════════════════════════════════╗" | lolcat
-echo -e " ║                   WIREGUARD                  ║" | lolcat
-echo -e " ╠══════════════════════════════════════════════╝" | lolcat
+echo -e " ╔════════━━━━━━━━━────── • ──────━━━━━━━━━════════╗" | lolcat
+echo -e " ║                     WIREGUARD                   ║" | lolcat
+echo -e " ╠════════━━━━━━━━━────── • ──────━━━━━━━━━════════╝" | lolcat
 echo -e " ║" | lolcat
-echo -e " ║  [ 1 ] Create Wireguard Account"
-echo -e " ║  [ 2 ] Delete Wireguard Account"
-echo -e " ║  [ 3 ] Check User Login Wireguard"
-echo -e " ║  [ 4 ] Extend Wireguard Account Active Life"
-echo -e " ║  [ 5 ] Check Wireguard User Interface"
+echo -e " ║  [ 1 ] > Create Wireguard Account"
+echo -e " ║  [ 2 ] > Delete Wireguard Account"
+echo -e " ║  [ 3 ] > Check User Login Wireguard"
+echo -e " ║  [ 4 ] > Extend Wireguard Account Active Life"
+echo -e " ║  [ 5 ] > Check Wireguard User Interface"
 echo -e " ║" | lolcat
-echo -e " ╠══════════════════════════════════════════════╗" | lolcat
-echo -e " ║ - CTRL C to cancel                           ║"   | lolcat
-echo -e " ╚══════════════════════════════════════════════╝" | lolcat
+echo -e " ╠════════━━━━━━━━━────── • ──────━━━━━━━━━════════╗" | lolcat
+echo -e " ║ - CTRL C to cancel                              ║"   | lolcat
+echo -e " ╚════════━━━━━━━━━────── • ──────━━━━━━━━━════════╝" | lolcat
 echo -e "\e[1;32m"
 read -p "     Please select an option :  "  wg
 echo -e "\e[0m"
