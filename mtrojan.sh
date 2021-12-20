27211dc03faf3d3ec0b51abe475e270712686c7c
XC0D3-X <harithsaufi0707@gmail.com>
2021-12-20 21:33:56 +0800

    Update mtrojan.sh

--- mtrojan.sh
+++ mtrojan.sh
@@ -10,20 +10,20 @@ if [ "$(systemd-detect-virt)" == "openvz" ]; then
 		exit 1
 fi
 clear
-
+echo -e  "  ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰" |lolcat
 echo -e ""
-echo -e  "  ╔═══════════════════════════════════════════════╗" | lolcat
-echo -e  "  ║                      TROJAN                   ║" | lolcat
-echo -e  "  ╠═══════════════════════════════════════════════╝" | lolcat
+echo -e  "  ╔════════━━━━━━━━━────── • ──────━━━━━━━━━════════╗" | lolcat
+echo -e  "  ║                      TROJAN                     ║" | lolcat
+echo -e  "  ╠════════━━━━━━━━━────── • ──────━━━━━━━━━════════╝" | lolcat
 echo -e  "  ║" | lolcat
-echo -e  "  ║  [ 1 ] Create Trojan Account"
-echo -e  "  ║  [ 2 ] Delete Trojan Account"
-echo -e  "  ║  [ 3 ] Extend Trojan Account Active Life"
-echo -e  "  ║  [ 4 ] Check User Login Trojan"
+echo -e  "  ║  [ 1 ] > Create Trojan Account"
+echo -e  "  ║  [ 2 ] > Delete Trojan Account"
+echo -e  "  ║  [ 3 ] > Extend Trojan Account Active Life"
+echo -e  "  ║  [ 4 ] > Check User Login Trojan"
 echo -e  "  ║" | lolcat
-echo -e  "  ╠═══════════════════════════════════════════════╗" | lolcat
-echo -e  "  ║ - CTRL C to cancel                            ║" | lolcat
-echo -e  "  ╚═══════════════════════════════════════════════╝" | lolcat
+echo -e  "  ╠════════━━━━━━━━━────── • ──────━━━━━━━━━════════╗" | lolcat
+echo -e  "  ║ - CTRL C to cancel                              ║" | lolcat
+echo -e  "  ╚════════━━━━━━━━━────── • ──────━━━━━━━━━════════╝" | lolcat
 echo -e "\e[1;32m"
 read -p "     Please select an option :  "  trojan
 echo -e "\e[0m"
